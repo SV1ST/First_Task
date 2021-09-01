e84a301ca09dc7719470a18a1e242c0d4674fe9a
SV1ST <kipernik1999@mail.ru>
2021-09-01 22:44:58 +0300

    load

---
+++ Calculator/Controllers/CalcController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Calculator.Models;
+
+namespace Calculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new calc());
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(calc c,string calculate)
+        {
+            if (calculate == "add")
+                c.tot = c.no1 + c.no2;
+            else if (calculate == "min")
+                c.tot = c.no1 - c.no2;
+            else if (calculate == "sub")
+                c.tot = c.no1 * c.no2;
+            else if (calculate == "divv" && c.no2 != 0)             
+                c.tot = c.no1 / c.no2;           
+            else
+                c.tot = c.no1 = c.no2 = default;
+
+            return View(c);
+        }
+            
+    }
+}
---
+++ Calculator/Models/calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+    public class calc
+    {
+        public double no1{ get; set; }
+
+        public double no2 { get; set; }
+
+        public double tot { get; set; }
+    }
+}
